abfd9cfdc25b97b8452e3b7e8a1de2d3991f4e51
David Disch <elijahdisch@gmail.com>
2021-09-01 23:01:18 +1000

    Change to output JSON

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
 #[macro_use] extern crate rocket;
 
+use rocket::serde::json::{Json};
+use rocket::serde::{Serialize};
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct Task {
+  name: String
+}
+
 #[get("/")]
-fn hello() -> &'static str {
-  "Hello, world!"
+fn todo() -> Json<Task> {
+    Json(Task { name: "Hello, world".to_string() })
 }
 
 #[launch]
 fn rocket() -> _ {
-  rocket::build().mount("/", routes![hello])
+  rocket::build().mount("/", routes![todo])
 }
